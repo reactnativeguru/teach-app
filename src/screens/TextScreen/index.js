69f7c216753bd732b942e171bef3894502a9ce70
‘John <john@reactnative.guru>
2019-10-23 19:13:02 +0100

    text with useState and onChangeText

--- src/screens/TextScreen/index.js
+++ src/screens/TextScreen/index.js
@@ -1,21 +1,38 @@
-import React from "react";
+import React, {useState} from "react";
 import { 
     View,
     Text,
-    StyleSheet
+    StyleSheet, TextInput
 } from "react-native";
 
-const TextScreen = (props) => (
-    <View style={styles.container}>
-        <Text>TextScreen</Text>
+const TextScreen = (props) => {
+    const [name, setName] = useState('')
+    return (
+        <View style={styles.container}>
+        <Text>Enter name</Text>
+        <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.input}
+        value={name}
+        onChangeText={(newValue) => setName(newValue) }
+
+        />
+        <Text>{name}</Text>
     </View>
     )
+}
+
+
 export default TextScreen;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
+     
+    },
+    input :{
+        margin: 15,
+        borderColor: 'black',
+        borderWidth: 1,
     }
 });
